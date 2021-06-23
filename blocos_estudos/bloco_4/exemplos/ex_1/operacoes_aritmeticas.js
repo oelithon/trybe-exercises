18a306726b339dd49b4c0425788c233d956714c6
Elithon Silva <elithonsilva@gmail.com>
2021-06-23 15:33:41 -0300

    tipos primitivos e operacoes aritmeticas

---
+++ blocos_estudos/bloco_4/exemplos/ex_1/operacoes_aritmeticas.js
@@ -0,0 +1,16 @@
+//+, -, *, /, **
+
+let salary = 3500;
+
+//console.log(salary + salary);
+//console.log(salary - salary);
+//console.log(salary * salary);
+//console.log(salary / salary);
+//console.log(3 ** 3);
+
+// ++ incrementar
+// -- decrementar
+
+salary++; //vai acrescentar +1 a essa variável
+
+console.log(salary);
