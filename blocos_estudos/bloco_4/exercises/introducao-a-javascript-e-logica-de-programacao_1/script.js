84de3f335eeafbda0b9285b0efba262edcedbb8e
Elithon Silva <elithonsilva@gmail.com>
2021-06-23 19:22:52 -0300

    Primeira Questao

---
+++ blocos_estudos/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -0,0 +1,56 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição
+
+/*
+let a;
+let b;
+let resultado;
+
+a = 5;
+b = 3;
+resultado = a + b;
+console.log(resultado);
+*/
+
+// Subtração
+
+/*
+let a = 5;
+let b = 3;
+let resultado;
+
+resultado = a - b;
+console.log(resultado);
+*/
+
+// Multiplicação
+
+/*
+let a = 5;
+let b = 3;
+let resultado = a * b;
+
+console.log(resultado);
+*/
+
+// Divisão
+
+/*
+let a = 5;
+let b = 2;
+let resultado = a / b;
+
+console.log(resultado);
+*/
+
+// Módulo (Resto)
+
+/*
+let a = 3;
+let b = 2;
+let resultado = a % b;
+
+console.log(resultado);
+*/
+
