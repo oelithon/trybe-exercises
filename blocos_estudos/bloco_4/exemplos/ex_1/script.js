3e69d00200bf37201e72e880ef7e0b5b697ef375
Elithon Silva <elithonsilva@gmail.com>
2021-06-23 15:12:50 -0300

    Aprendendo sobre constantes e variaveis

---
+++ blocos_estudos/bloco_4/exemplos/ex_1/script.js
@@ -0,0 +1,15 @@
+const name = "Elithon";
+const birthCity = "Pedreiras";
+let birthYear = 1995;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Trizidela";
+
+console.log(birthCity);
