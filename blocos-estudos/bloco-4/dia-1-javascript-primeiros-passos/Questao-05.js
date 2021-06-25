1c09c05999ea8a4849045b9d746b0302d615d743
Elithon Silva <elithonsilva@gmail.com>
2021-06-25 12:37:46 -0300

    Comecei a questao-05

---
+++ blocos-estudos/bloco-4/dia-1-javascript-primeiros-passos/Questao-05.js
@@ -0,0 +1,10 @@
+//5. Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+//Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+
+let angleA = 45;
+let angleB = 90;
+let angleC = 45;
+
+for (let index = angleA + angleB + angleC) {
+}
