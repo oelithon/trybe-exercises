4c0128b59c461ac90706d8cde3ec8922cee2a137
Elithon Silva <elithonsilva@gmail.com>
2021-06-25 18:51:14 -0300

    Questao-06

---
+++ blocos-estudos/bloco-4/dia-2-javascript-array-e-loop-for/Questao-06.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let valoresImpares = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        valoresImpares += 1;
+    }
+}
+
+if (valoresImpares === 0) {
+    console.log("nenhum valor ímpar encontrado.")
+} else {
+    console.log(valoresImpares);   
+}
