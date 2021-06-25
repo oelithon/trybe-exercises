37410b87dd02cd8cd93ba020ba569f35c18ac24b
Elithon Silva <elithonsilva@gmail.com>
2021-06-24 21:21:36 -0300

    Questao-02

---
+++ blocos-estudos/bloco-4/dia-1-javascript-primeiros-passos/Questao-02.js
@@ -0,0 +1,11 @@
+//2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let valor1 = 4;
+let valor2 = 8;
+let resultado;
+
+if (valor1 > valor2) {
+    console.log(resultado = valor1);
+} else {
+    console.log(resultado = valor2);
+}
