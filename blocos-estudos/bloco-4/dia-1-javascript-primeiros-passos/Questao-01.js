7b06f5ea011a0701834d4d5e9709126391359be5
Elithon Silva <elithonsilva@gmail.com>
2021-06-24 21:12:10 -0300

    Questao-01

---
+++ blocos-estudos/bloco-4/dia-1-javascript-primeiros-passos/Questao-01.js
@@ -0,0 +1,12 @@
+//1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição
+
+let a;
+let b;
+let resultado;
+
+a = 5;
+b = 3;
+resultado = a + b;
+console.log(resultado);
