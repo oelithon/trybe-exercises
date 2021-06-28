a2bca05ff1a43fcb8a9e020f6b4eedee63787c56
Elithon Silva <elithonsilva@gmail.com>
2021-06-28 18:48:17 -0300

    Atualizando Questao-03

--- blocos-estudos/bloco-4/dia-4-javascript-objetos-e-funcoes/Questao-03.js
+++ blocos-estudos/bloco-4/dia-4-javascript-objetos-e-funcoes/Questao-03.js
@@ -6,15 +6,6 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-//2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
-  //Valor esperado no console:
-  // {
-  //   personagem: 'Margarida',
-  //   origem: 'Pato Donald',
-  //   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  //   recorrente: 'Sim'
-  // };
-
 info.recorrente = 'Sim';
 
 //3. Faça um for/in que mostre todas as chaves do objeto.
