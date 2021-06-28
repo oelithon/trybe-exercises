b8a4f30715d6ecee5e0f1342e3fcec6dcf63b883
Elithon Silva <elithonsilva@gmail.com>
2021-06-28 19:01:29 -0300

    Questao-05

---
+++ blocos-estudos/bloco-4/dia-4-javascript-objetos-e-funcoes/Questao-05.js
@@ -0,0 +1,20 @@
+//Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+console.log(info.recorrente + ' e ' + info2.recorrente);
