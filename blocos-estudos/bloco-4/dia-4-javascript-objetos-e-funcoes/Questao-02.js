d9aea91327f9efee1edd68de7723a20335f34577
Elithon Silva <elithonsilva@gmail.com>
2021-06-28 18:42:33 -0300

    Questao-02

---
+++ blocos-estudos/bloco-4/dia-4-javascript-objetos-e-funcoes/Questao-02.js
@@ -0,0 +1,20 @@
+//Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+  //Valor esperado no console:
+  // {
+  //   personagem: 'Margarida',
+  //   origem: 'Pato Donald',
+  //   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  //   recorrente: 'Sim'
+  // };
+
+  info.recorrente = 'Sim';
+
+  console.log(info);
