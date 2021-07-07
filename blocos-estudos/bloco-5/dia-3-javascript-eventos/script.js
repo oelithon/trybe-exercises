943bed5739c2079ae4efad4f17537c0c370f7f9e
Elithon Silva <elithonsilva@gmail.com>
2021-07-07 19:05:50 -0300

    pequenas alteraçoes - exercicio nao finalizado

--- blocos-estudos/bloco-5/dia-3-javascript-eventos/script.js
+++ blocos-estudos/bloco-5/dia-3-javascript-eventos/script.js
@@ -14,3 +14,18 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function diasDoMes () {
+  let listaDeDias = document.querySelector('#days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let dias = dezDaysList[index];
+    
+    let listaDias = document.createElement('li');
+    listaDias.innerHTML = dias;
+
+    listaDeDias.appendChild(listaDias);
+  };
+};
