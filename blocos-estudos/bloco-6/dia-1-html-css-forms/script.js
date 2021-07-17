e04d67ae23547df18526fcb054e13a1a1ab06126
Elithon Silva <elithonsilva@gmail.com>
2021-07-17 14:16:26 -0300

    metade do exercicio

---
+++ blocos-estudos/bloco-6/dia-1-html-css-forms/script.js
@@ -0,0 +1,13 @@
+// Gerar UF
+const estado = document.querySelector('#estado');
+
+let uf = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+for (let index = 0; index < uf.length; index += 1) {
+  estado.appendChild(document.createElement('option')).innerText = uf[index];
+}
+
+const moment = require('moment');
+const valueDate = moment('17072021', 'DDMMYYYY')
+
+console.log(valueDate);
