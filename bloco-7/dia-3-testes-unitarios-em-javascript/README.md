833999938b8a444c1389c511527d234e53a28dd9
Elithon Silva <elithonsilva@gmail.com>
2021-07-26 18:34:18 -0300

    adiciona README.md e arquivo irpf.js com exercicio extra

---
+++ bloco-7/dia-3-testes-unitarios-em-javascript/README.md
@@ -0,0 +1,6 @@
+Exercícios 7.3:
+
+
+Exercício Extra:
+- [] No arquivo irpf.js. Escreva uma função que, dado um valor de salário, retorne a alíquota a ser aplicada para descontar o IR.
+- [] Escreva os cenários de teste.
---
+++ bloco-7/dia-3-testes-unitarios-em-javascript/irpf.js
@@ -0,0 +1,9 @@
+// Base de cálculo            Alíquota    Dedução
+// de 0,00 até 1.903,98       isento      0,00
+// de 1.903,99 até 2.826,65   7,50%	      142,80
+// de 2.826,66 até 3.751,05   15,00%      354,80
+// de 3.751,06 até 4.664,68   22,50%      636,13
+// a partir de 4.664,68       27,50%      869,36
+
+// Escreva uma função que, dado um valor de salário, retorne a alíquota a ser
+// aplicada para descontar o IR. Escreva os cenários de teste.
