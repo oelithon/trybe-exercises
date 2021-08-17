0a1efab722dac947b73cf8dc2c1eb0743091f7b6
Elithon Silva <elithonsilva@gmail.com>
2021-08-17 20:37:04 -0300

    impl: teste 02/04 da função sum()

--- bloco-10-dia-1-primeiros-passos-no-jest/src/sum.js
+++ bloco-10-dia-1-primeiros-passos-no-jest/src/sum.js
@@ -1,4 +1,12 @@
-const sum = (a, b) => a + b;
+const sum = (a, b) => {
+  if (typeof a === 'number' || typeof b === 'number') {
+    return a + b;
+  }
+
+  throw new Error('os parametros devem ser do tipo number')
+};
+
+console.log(sum(1, '2'));
 
 module.exports = {
   sum,
--- bloco-10-dia-1-primeiros-passos-no-jest/test/sum.spec.js
+++ bloco-10-dia-1-primeiros-passos-no-jest/test/sum.spec.js
@@ -3,3 +3,6 @@ const { sum } = require('../src/sum');
 test('Testa se o retorno de sum(4, 5) é 9', () => {
   expect(sum(4, 5)).toEqual(9);
 });
+test('Testa se o retorno de sum(0, 0) é 0', () => {
+  expect(sum(0, 0)).toEqual(0);
+});
