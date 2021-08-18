37e1d3e67947ff3b43f391aaf2858bb6553ac7ea
Elithon Silva <elithonsilva@gmail.com>
2021-08-18 19:35:20 -0300

    impl: teste para funcao uppercase

--- bloco-10-dia-2-jest-testes-assincronos/test/uppercase.spec.js
+++ bloco-10-dia-2-jest-testes-assincronos/test/uppercase.spec.js
@@ -1,4 +1,17 @@
+const { describe } = require('@jest/globals');
 const { uppercase } = require('../src/uppercase');
 
 // 1 - Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas.
 
+describe('Verifica se função retorna palavra com letras maiúsculas.', () => {
+  it('', (done) => {
+    uppercase('Elithon', (str) => {
+      try {
+        expect(str).toBe('ELITHON');
+        done();
+      } catch (error) {
+        done(error)
+      }
+    });
+  });
+});
