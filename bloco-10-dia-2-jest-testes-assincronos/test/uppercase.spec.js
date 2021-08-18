c564a1efb72462a042bf49cab8f648c30ea0ab55
Elithon Silva <elithonsilva@gmail.com>
2021-08-18 18:47:45 -0300

    Iniciando exercicios dia 10.2

---
+++ bloco-10-dia-2-jest-testes-assincronos/test/uppercase.spec.js
@@ -0,0 +1,4 @@
+const { uppercase } = require('../src/uppercase');
+
+// 1 - Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas.
+
